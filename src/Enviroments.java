45aaae4d333e87feeb156ee6ab979d13ff4378d6
Nick (Macbook) <nicksayswatzup@gmail.com>
2019-02-08 12:59:25 -0600

    work on extend

--- src/Enviroments.java
+++ src/Enviroments.java
@@ -1,20 +1,32 @@
 //Written by Nick Martin for CS403 - Programming Languages
 
-import java.io.IOException;
-
 public class Enviroments implements Types {
-
-    static Lexeme CurrentLexeme;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         System.out.println("Creating new enviroment");
-        Lexeme EnviromentTree = create();
-        printAllEnviroments(EnviromentTree);
+        Lexeme GlobalTree = create();
+        printAllEnviroments(GlobalTree);
         System.out.println("Inserting variable \'x\' with value 3");
         Lexeme idLexeme = new Lexeme(VARIABLE, 'x', 0);
         Lexeme valLexeme = new Lexeme(INTEGER, 3, 0);
-        insert(idLexeme, valLexeme, EnviromentTree);
-        printAllEnviroments(EnviromentTree);
+        insert(idLexeme, valLexeme, GlobalTree);
+        idLexeme = new Lexeme(VARIABLE, "zz", 0);
+        valLexeme = new Lexeme(INTEGER, 55, 0);
+        insert(idLexeme, valLexeme, GlobalTree);
+
+        printAllEnviroments(GlobalTree);
+
+        // System.out.println("Extending the environment with y:4 and z:\"hello\"");
+        // Lexeme localTree = create();
+        // idLexeme = new Lexeme(VARIABLE, 'y', 0);
+        // valLexeme = new Lexeme(INTEGER, 4, 0);
+        // insert(idLexeme, valLexeme, GlobalTree);
+        // idLexeme = new Lexeme(VARIABLE, 'z', 0);
+        // valLexeme = new Lexeme(STRING, "hello", 0);
+        // insert(idLexeme, valLexeme, localTree);
+
+        // extend(localTree.car().car(), localTree.car().cdr(), GlobalTree);
+        // printAllEnviroments(GlobalTree);
+
 
         System.exit(0);
     }
@@ -26,7 +38,7 @@ public class Enviroments implements Types {
     }
 
     static Lexeme extend(Lexeme variables, Lexeme values, Lexeme env) {
-        return cons(ENV, variables, cons(ENV, values, env));
+        return cons(ENV,cons(TABLE ,variables, values), env);
     }
     
     static Lexeme create() {
@@ -42,7 +54,7 @@ public class Enviroments implements Types {
 
     static void printAllEnviroments(Lexeme tree) {
         while (tree != null) {
-            System.out.println("The enviroment is:");
+            System.out.println("The local enviroment is:");
             Lexeme table = tree.car();
             Lexeme currVar = table.car();
             Lexeme currVal = table.cdr();
@@ -53,10 +65,9 @@ public class Enviroments implements Types {
                 currVal = currVal.cdr();
                 currVar = currVar.cdr();
             }
-
-
             tree = tree.cdr();
         }
+        System.out.println();
     }
 
     static void printLocalEnviroment(Lexeme tree) {
